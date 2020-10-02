6333914827827f35aab7a8c7115f35b7c2d85952
Phongphisut0812 <68526077+Phongphisut0812@users.noreply.github.com>
2020-10-02 23:08:13 +0700

    at

---
+++ pages/shop/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>รายละเอียด</h1>
+    <h2>สินค้าหมายเลข {{ this.$route.params.id }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  Data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+ 
+};
+</script>
+
+<style>
+</style>
